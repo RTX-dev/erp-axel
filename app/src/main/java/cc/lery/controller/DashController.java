a80c2679a5f1449f3f4d54cbbf798a65ec0ba7fb
Evan Petitbon <evan.petitbon@gmail.com>
2025-05-26 17:20:08 +0200

    mise a jour de la page de connection

--- app/src/main/java/cc/lery/controller/DashController.java
+++ app/src/main/java/cc/lery/controller/DashController.java
@@ -2,30 +2,23 @@ package cc.lery.controller;
 
 import java.io.IOException;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class DashController {
 
-        @FXML
-    private TextField fieldlName;
 
     @FXML
-    private Text repeatName;
-
-    @FXML
-    public void BackToLogin(ActionEvent event)
+    public void BackToLogin(MouseEvent event)
     {
         try {
             //charger le fichier FXML de la deuxième page
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/home_page.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/page-connexion.fxml"));
             Parent root = loader.load();
 
             //Créer une nouvelle scène avec le contenu de la deuxième page
@@ -33,11 +26,7 @@ public class DashController {
 
             //Obtenir la scène actuelle à partir de l'événement
             Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-            //En decomposant le code ci-dessus on pourrait écrire :
-            //Node node = (Node) event.getSource();
-            //Scene scene = node.getScene();
-            //Stage stage = (Stage) scene.getWindow();
-
+            
             //Définir la nouvelle scène sur le stage
             stage.setScene(scene);
             stage.show();
